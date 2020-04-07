783e5361a85402d758e0f74a6264643304ff54bd
Thomas James <tom.james@data-summit.co.uk>
2020-04-07 16:36:47 +0100

    New Projects Created for SQL Classes
    Entry project (console app) allows settings and configuration setup for testing.
    
    XmlSchemaSQLClassGenerator will handle the SQL code generation, which is separate to the C# class generation.

--- XmlSchemaClassGenerator/FileOutputWriter.cs
+++ XmlSchemaClassGenerator/FileOutputWriter.cs
@@ -1,6 +1,9 @@
-﻿using System.CodeDom;
+﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace XmlSchemaClassGenerator
 {
@@ -30,10 +33,16 @@ namespace XmlSchemaClassGenerator
             var cu = new CodeCompileUnit();
             cu.Namespaces.Add(cn);
 
-            var path = Path.Combine(OutputDirectory, cn.Name + ".cs");
-            Configuration?.WriteLog(path);
-
-            WriteFile(path, cu);
+            if (Configuration.SeparateClasses == true)
+            {
+                WriteSeparateFiles(cn);
+            }
+            else
+            {
+                var path = Path.Combine(OutputDirectory, cn.Name + ".cs");
+                Configuration?.WriteLog(path);
+                WriteFile(path, cu);
+            }
         }
 
         protected virtual void WriteFile(string path, CodeCompileUnit cu)
@@ -55,6 +64,53 @@ namespace XmlSchemaClassGenerator
                 if (fs != null)
                     fs.Dispose();
             }
+        }
+
+        private void WriteSeparateFiles(CodeNamespace cn)
+        {
+            try
+            {
+                string dirPath = Path.Combine(OutputDirectory, ValidateName(cn.Name));
+                DirectoryInfo di = null;
+                //Create directory to hold the output files
+                if (Directory.Exists(dirPath))
+                {
+                    di = Directory.GetParent(dirPath);
+                }
+                else
+                {
+                    di = Directory.CreateDirectory(dirPath);
+                }
+
+
+                var ccu = new CodeCompileUnit();
+                var cns = new CodeNamespace(ValidateName(cn.Name));
+
+                cns.Imports.AddRange(cn.Imports.Cast<CodeNamespaceImport>().ToArray());
+                cns.Comments.AddRange(cn.Comments);
+                ccu.Namespaces.Add(cns);
+
+                foreach (CodeTypeDeclaration ctd in cn.Types)
+                {
+                    string path = Path.Combine(dirPath, ctd.Name + ".cs");
+                    cns.Types.Clear();
+                    cns.Types.Add(ctd);
+                    Configuration?.WriteLog(path);
+                    WriteFile(path, ccu);
+                }
+            }
+            catch (Exception ae)
+            {
+                string s = ae.ToString();
+                if (ae.InnerException != null) s = ae.InnerException.Message;
+            }
+        }
+
+        private string ValidateName(string name)
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            string validName = Regex.Replace(name, regexSearch, "_");
+            return validName.Replace(".", "_");
         }
     }
 }
---
+++ XmlSchemaClassGenerator/Validation/Namespace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace XmlSchemaClassGenerator.Validation
+{
+    public static class Namespace
+    {
+        public static string NameIsValid(string name)
+		{
+            string validName = name;
+			try
+			{
+                string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+                validName = Regex.Replace(name, regexSearch, "_");
+                validName = validName.Replace(".", "_");
+            }
+            catch (Exception ae)
+            {
+                string s = ae.ToString();
+                if (ae.InnerException != null) s = ae.InnerException.Message;
+                validName = "";
+            }
+            return validName;
+        }
+    }
+}
